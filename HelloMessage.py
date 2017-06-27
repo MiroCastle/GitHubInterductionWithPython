45e7af8ce64cbd7873927cf8a5d103e36e2c4db7
Sebastian <spiegseb@students.zhaw.ch>
2017-06-27 15:45:06 +0200

    Initial commit

---
+++ HelloMessage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+import sys
+
+print("Hello")
+if len(sys.argv) == 2:
+    result = len(sys.argv[1])
+    parameterWord = sys.argv[1]
+    if result <= 0:
+        print("length eqauls zero")
+
+    print("The enter parameter is called")
+    print(parameterWord)
+else:
+    print("Not Right amount of parameters")
+
+
+
